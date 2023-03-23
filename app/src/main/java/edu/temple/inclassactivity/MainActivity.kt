124325245204370d7cbd09bf41d4b05be6234efa
Jason Michel <tuk94727@temple.edu>
2023-03-23 15:05:58 -0400

    Completed inclass activity

--- app/src/main/java/edu/temple/inclassactivity/MainActivity.kt
+++ app/src/main/java/edu/temple/inclassactivity/MainActivity.kt
@@ -2,24 +2,30 @@ package edu.temple.inclassactivity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.lifecycle.ViewModelProvider
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        val mainViewModel = ViewModelProvider(this)[MainViewModel::class.java]
         // Fetch images into IntArray called imageArray
         val typedArray = resources.obtainTypedArray(R.array.image_ids)
         val imageArray = IntArray(typedArray.length()) {typedArray.getResourceId(it, 0)}
         typedArray.recycle()
 
+        mainViewModel.setImageIds(imageArray)
+
         // Attach an instance of ImageDisplayFragment using factory method
 
-        val fragment1 = ImageDisplayFragment.newInstance(imageArray)
-
-        val fragmentTransaction = supportFragmentManager.beginTransaction()
-        fragmentTransaction.add(R.id.fragmentContainerView, fragment1)
-        fragmentTransaction.commit()
-
+        if (supportFragmentManager.findFragmentById(R.id.fragmentContainerView) !is ImageDisplayFragment)
+            supportFragmentManager
+                .beginTransaction()
+                .add(R.id.fragmentContainerView, ImageDisplayFragment())
+                .addToBackStack(null)
+                .commit()
     }
+
+
 }
